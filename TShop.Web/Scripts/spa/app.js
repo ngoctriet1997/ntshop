e6a43747f6722a3d05d6cdb6d2ed5d2d08420bd6
ngoctriet <ngoctriet201297@gmail.com>
2018-12-28 21:10:51 +0700

    what is scope and rootescope?

---
+++ TShop.Web/Scripts/spa/app.js
@@ -0,0 +1,9 @@
+﻿var myApp = angular.module("myModule", []);
+
+myApp.controller("myController", myController);
+
+myController.$inject = ['$scope'];
+function myController($rootscope,$scope) {
+
+    $rootscope.message = "this is my message from controller";
+}
