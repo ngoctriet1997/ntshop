6755f7f0f9156cbb18d2db478bdcd3f76b838313
ngoctriet <ngoctriet201297@gmail.com>
2018-12-30 23:19:41 +0700

    config single page

---
+++ TShop.Web/app/components/products/products.module.js
@@ -0,0 +1,15 @@
+﻿(function (app) {
+    angular.module('tshop.products', ['tshop.common']).config(config);
+    config.$inject = ['$stateProvider', '$urlRouterProvider'];
+    function config($stateProvider, $urlRouterProvider) {
+        $stateProvider.state('products', {
+            url: "/products",
+            templateUrl: "/app/components/products/productListView.html",
+            controller: "productListController"
+        }).state('product_add', {
+            url: "/product_add",
+            templateUrl: "/app/components/products/productAddView.html",
+            controller: "productAddController"
+        })
+    }
+})();
